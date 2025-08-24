5023ea85a0c8759d14dfee1a25a6bb34956bd637
JonathamCarvalho <jonatham.carvalho@gmail.com>
2025-08-24 18:01:24 -0300

    feat: add mobile pagination

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { userCars } from "../hooks/useCars";
 import { CarCard } from "./CarCard";
 import styles from "../../public/css/home.module.css";
 import { Text } from "vcc-ui";
 import { Spacer } from "./Spacer";
 import PaginationDesktop from "./PaginationDesktop";
+import PaginationMobile from "./PaginationMobile";
 
 export const HomeCompent: React.FC = () => {
   const cars = userCars();
+  const [selected, setSelected] = useState(0);
 
   const onClickLeft = () => {
     let CardList = document.getElementById("card-list");
@@ -30,6 +32,17 @@ export const HomeCompent: React.FC = () => {
     else CardList?.scrollTo({ left: scrollPosition + cardSize });
   };
 
+  const onClickMobile = (index: number) => {
+    let CardList = document.getElementById("card-list");
+    let card = CardList?.firstElementChild;
+    let cardSize = (card?.clientWidth ?? 0) + 24;
+    let scrollSize = CardList?.scrollWidth ?? 0;
+    let scrollPosition = CardList?.scrollLeft ?? 0;
+
+    CardList?.scrollTo({ left: scrollPosition + cardSize * index });
+    setSelected(index);
+  };
+
   return (
     <div className={styles.homeWrapper}>
       <Text variant="cook">Modelos em destaque</Text>
@@ -43,6 +56,11 @@ export const HomeCompent: React.FC = () => {
         onClickLeft={() => onClickNavigate(true)}
         onClickRight={() => onClickNavigate(false)}
       />
+      <PaginationMobile
+        selected={selected}
+        onClick={onClickMobile}
+        total={cars.length}
+      ></PaginationMobile>
     </div>
   );
 };
---
+++ src/components/PaginationMobile.tsx
@@ -0,0 +1,25 @@
+import styles from "../../public/css/paginationMobile.module.css";
+
+interface PaginationMobileProps {
+  total: number;
+  onClick: (i: number) => void;
+  selected: number;
+}
+
+export default function PaginationMobile({
+  total,
+  onClick,
+  selected,
+}: PaginationMobileProps) {
+  return (
+    <div className={styles.btnWrapper}>
+      {Array.from({ length: total }).map((Element, i) => (
+        <button
+          className={i === selected ? styles.btnSelected : styles.btn}
+          key={i}
+          onClick={() => onClick(i)}
+        ></button>
+      ))}
+    </div>
+  );
+}
