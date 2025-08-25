a2d5f1e62bf4458798817e7ba54bedb4610edbfe
JonathamCarvalho <jonatham.carvalho@gmail.com>
2025-08-25 19:24:50 -0300

    fix: use static JSON data for cars instead of API

--- src/hooks/useCars.ts
+++ src/hooks/useCars.ts
@@ -1,14 +1,15 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 import { Car } from "../types/car.interface";
+import cars from "../../public/api/cars.json";
 
-export function useCars() {
-  const [cars, setCars] = useState<Car[]>([]);
+export function useCars(): Car[] {
+  // const [cars, setCars] = useState<Car[]>([]);
 
-  useEffect(() => {
-    axios.get(process.env.NEXT_PUBLIC_API_URL + "/api/cars").then((res) => {
-      setCars(res.data);
-    });
-  }, []);
+  // useEffect(() => {
+  //   axios.get(process.env.NEXT_PUBLIC_API_URL + "/api/cars").then((res) => {
+  //     setCars(res.data);
+  //   });
+  // }, []);
   return cars;
 }
